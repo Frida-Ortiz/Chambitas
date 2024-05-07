1348ca05531e7277855982739b0d80bee31e0575
SasoHdz <isaacrdzcmd@gmail.com>
2024-05-06 22:52:30 -0600

    Configuracion de rutas y separacion de paginas

--- chambitas/src/app/app.routes.ts
+++ chambitas/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { RegistroComponent } from './pages/registro/registro.component';
+import { OfertatrabajoComponent } from './pages/ofertatrabajo/ofertatrabajo.component';
+import { LayoutComponent } from './pages/layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'registro', component: RegistroComponent },
+  { path: 'oferta-trabajo', component: OfertatrabajoComponent },
+  { path: 'layout', component: LayoutComponent }
+
+];
---
+++ chambitas/src/app/pages/ofertatrabajo/ofertatrabajo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ofertatrabajo',
+  standalone: true,
+  imports: [],
+  templateUrl: './ofertatrabajo.component.html',
+  styleUrl: './ofertatrabajo.component.css'
+})
+export class OfertatrabajoComponent {
+
+}
