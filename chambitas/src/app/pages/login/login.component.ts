678a6a4f37466bc22f00f5986d74398739fe7c51
SasoHdz <isaacrdzcmd@gmail.com>
2024-05-07 23:36:30 -0600

    Configurando autenticacion

--- chambitas/src/app/pages/login/login.component.ts
+++ chambitas/src/app/pages/login/login.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { SupabaseService } from '../../services/supabase.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  email: string = '';
+  pass: string = '';
+
+  constructor(private supabaseService: SupabaseService) {}
+
+  async registerUser() {
+    const email = 'example@email.com';
+    const password = 'example-password';
+  const response = await this.supabaseService.signUp(email, password);
+    if (response.error) {
+      console.error('Registration failed:', response.error);
+    } else if (response.data) {
+      console.log('User registered:', response.data);
+    }
+  }
 
 }
--- chambitas/src/app/services/supabase.service.ts
+++ chambitas/src/app/services/supabase.service.ts
@@ -1,6 +1,6 @@
 // src/app/services/supabase.service.ts
 import { Injectable } from '@angular/core';
-import { createClient, SupabaseClient } from '@supabase/supabase-js';
+import { createClient, SupabaseClient,AuthResponse, AuthSession } from '@supabase/supabase-js';
 import { environment } from '../evironments/environment';
 
 @Injectable({
@@ -15,7 +15,26 @@ export class SupabaseService {
     this.supabase = createClient(this.supabaseUrl, this.supabaseAnonKey);
   }
 
+
+  async signUp(email: string, password: string): Promise<AuthResponse> {
+    const response: AuthResponse = await this.supabase.auth.signUp({
+      email: email,
+      password: password,
+    });
+    return response;
+  }
+
+  async signIn(email: string, password: string): Promise<AuthResponse> {
+    const response: AuthResponse = await this.supabase.auth.signInWithPassword({
+      email: email,
+      password: password,
+    });
+    return response;
+  }
+
+
   get client(): SupabaseClient {
     return this.supabase;
   }
+
 }
