a1765528fe91fbc115505d37b60cc76eddffe4ad
SasoHdz <isaacrdzcmd@gmail.com>
2024-05-08 19:41:13 -0600

    Formulario crear oferta trabajo

--- chambitas/src/app/pages/ofertatrabajo/ofertatrabajo.component.ts
+++ chambitas/src/app/pages/ofertatrabajo/ofertatrabajo.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common'; // Importa CommonModule
 
 @Component({
   selector: 'app-ofertatrabajo',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './ofertatrabajo.component.html',
   styleUrl: './ofertatrabajo.component.css'
 })
 export class OfertatrabajoComponent {
+  registro: any = {
+    nombreTrabajo: '',
+    descripcionTrabajo: '',
+    actividades: '',
+    pago: null,
+    oficios: '',
+    tipoContrato: ''
+  };
 
+  submitForm() {
+    // Aquí puedes manejar la lógica para enviar el formulario
+    console.log('Formulario enviado:', this.registro);
+  }
 }
