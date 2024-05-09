bf457550540b1500f37e435c001eda0115dbe351
SasoHdz <isaacrdzcmd@gmail.com>
2024-05-08 20:07:24 -0600

    Trabajando en oferta trabajo

--- chambitas/src/app/pages/ofertatrabajo/ofertatrabajo.component.ts
+++ chambitas/src/app/pages/ofertatrabajo/ofertatrabajo.component.ts
@@ -10,6 +10,9 @@ import { CommonModule } from '@angular/common'; // Importa CommonModule
   styleUrl: './ofertatrabajo.component.css'
 })
 export class OfertatrabajoComponent {
+
+  oficios: [string] = ['Vacio'];
+
   registro: any = {
     nombreTrabajo: '',
     descripcionTrabajo: '',
@@ -18,6 +21,18 @@ export class OfertatrabajoComponent {
     oficios: '',
     tipoContrato: ''
   };
+
+  onChangeOficio(event: any) {
+    // const oficioSeleccionado = event?.target?.value;
+    // if (oficioSeleccionado) {
+    //   console.log('Oficio seleccionado:', oficioSeleccionado);
+    //   this.oficios.push(oficioSeleccionado)
+    //   // Puedes realizar cualquier lógica adicional aquí
+    // } else {
+    //   console.error('Error: No se pudo obtener el valor del select.');
+    // }
+    console.log(event)
+  }
 
   submitForm() {
     // Aquí puedes manejar la lógica para enviar el formulario
